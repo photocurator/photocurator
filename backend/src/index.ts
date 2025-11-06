b0bdaac51a4b760ae96c0e5e294fa2eefa70e682
Party4Bread <party4bread@gmail.com>
2025-11-06 12:34:31 +0900

    fix: for type safety on better-auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,31 @@
 import { Hono } from 'hono'
-import { auth } from './lib/auth';
+import { auth, AuthType } from './lib/auth';
 
-const app = new Hono()
+
+type Variables = {
+  user: AuthType['user']
+  session: AuthType['session']
+}
+
+const app = new Hono<{ Variables: Variables }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  	if (!session) {
+    	c.set("user", null);
+    	c.set("session", null);
+    	await next();
+        return;
+  	}
+  	c.set("user", session.user);
+  	c.set("session", session.session);
+  	await next();
+});
+
 
 app.on(["POST", "GET"], "/api/auth/*", (c) => {
 	return auth.handler(c.req.raw);
